0037a6ce4e210d3ba3b2f9045b7007f91227f3fd
David Garibaldi <david.c.garibaldi@outlook.com>
2021-02-21 12:46:28 -0500

    Completed variables exercise

---
+++ Variables.py
@@ -0,0 +1,17 @@
+#Numeric Variables
+x = 2
+x
+_+3 # will take the output from last line as input
+
+
+#String Variables
+String='David'
+    #Combine a varible with another string
+String + " is awesome!"
+#taking a part of a string
+String[0] # will retrieve the first letter in the string
+#Take a range in the string
+String[2:] # returns everything from the 3rd to last letter
+String[2:4] # will return everything between 3rd and 4th letter
+String[-1] # will return the last letter in the string
+
