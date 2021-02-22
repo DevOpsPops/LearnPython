066b5beacae78a547c292705ad7b09d44fbcf490
David Garibaldi <david.c.garibaldi@outlook.com>
2021-02-21 19:24:13 -0500

    Completed exercise Previous Cmd and Clear Screen

---
+++ IDLEPreviousCommandClearScreen.py
@@ -0,0 +1,6 @@
+#To run the previous command - press the 'up' arrow
+
+#To interrupt a script that is running - press 'ctrl+c'
+
+#To clear the screen - you will need to end session and create a new one
+#by clicking the trashcan and then running a command/code again
