2dddc98b6bf6423b66f41e4f37c0423f19302929
DevOpsPops <48504125+DevOpsPops@users.noreply.github.com>
2021-02-21 21:24:34 -0500

    Revert "Fundementals"

--- PreviousCommandClearScreen.py
+++
@@ -1,6 +0,0 @@
-#To run the previous command - press the 'up' arrow
-
-#To interrupt a script that is running - press 'ctrl+c'
-
-#To clear the screen - you will need to end session and create a new one
-#by clicking the trashcan and then running a command/code again
