f5c64dfb4be9fb687d6fb44d20e4c2e26cfde233
David Garibaldi <david.c.garibaldi@outlook.com>
2021-02-23 17:42:01 -0500

    Completed Excersie RunDebug

---
+++ RunDebugTrace.py
@@ -0,0 +1,13 @@
+#Using an IDE has several Advantages
+
+#Dubug
+    #Utilize breakpoints to see what is happening at each indicated point
+    #see code below for example
+
+x = 5 # place a break here
+y = 8
+z = x + y
+print(z)
+
+#if you Start debugging code will pause running at the break.
+#you can then manually run each line to verify each step
