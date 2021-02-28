d1e8ed6daec39341a68cca2ca9a3f3761c60c648
David Garibaldi <david.c.garibaldi@outlook.com>
2021-02-28 11:44:40 -0500

    Install Numpy in progress

---
+++ InstallNumpy.py
@@ -0,0 +1,16 @@
+#Types of Arrays:
+    #Single Demensional Array
+        #there is only one row and multiple columns
+    #Multi-Dementinal Array
+        #there are multiple rows and multiple columns
+            #you can not use the array package to do this, instead you will need to import from numpy
+
+
+import numpy as np
+
+#by default, numpy is not a package that comes with Python
+#we will need to install it by using PIP through the command prompt
+
+#we will need to install pip first
+#run the following in cmd:
+    #python -m pip install numpy
